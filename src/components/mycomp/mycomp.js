764889e16cca63eaa0611bcf3cada2ce56525681
David Wieczorek <blackratio@gmail.com>
2017-08-14 12:09:08 +0200

    adding loading spinner and error handling

--- src/components/mycomp/mycomp.js
+++ src/components/mycomp/mycomp.js
@@ -4,31 +4,51 @@ import moment from 'moment';
 const API = 'https://hn.algolia.com/api/v1/search?query=';
 const DEFAULT_QUERY = 'redux';
 
-
 class MyComp extends Component {
    constructor(props) {
       super(props);
 
       this.state = {
-         hits: []
+         hits: [],
+         isLoading: false,
+         error: null
       };
    }
 
    componentDidMount() {
+      this.setState({ isLoading: true });
       fetch(API + DEFAULT_QUERY)
-         .then(response => response.json())
-         .then(data => this.setState({ hits: data.hits }));
+         .then(response => {
+            if (response.ok) {
+               return response.json();
+            } else {
+               throw new Error('Something went wrong ...');
+            }
+         })
+         .then(data => this.setState({ hits: data.hits, isLoading: false }))
+         .catch(error => this.setState({ error, isLoading: false }));
    }
 
    render() {
-      const { hits } = this.state;
+      const { hits, isLoading, error } = this.state;
 
+      if (error) {
+         return (
+            <p>
+               {error.message}
+            </p>
+         );
+      }
+      if (isLoading) {
+         return <p>Loading ...</p>;
+      }
       return (
          <div className="data-bloc">
             {hits.map(hit =>
                <section key={hit.objectID}>
                   <h4>
-                     {hit.author} - {moment(hit.created_at).format('DD/MM/YYYY')}
+                     {hit.author} -{' '}
+                     {moment(hit.created_at).format('DD/MM/YYYY')}
                   </h4>
                   <a href={hit.url}>
                      {hit.title}
