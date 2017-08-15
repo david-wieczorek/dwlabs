b58158c5c7f563e85254ce720733443f000a018b
David Wieczorek <blackratio@gmail.com>
2017-08-15 17:12:47 +0200

    test change index

--- src/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './components';
-
-describe('Init page suite', () => {
-   it('renders App without crashing', () => {
-      shallow(<App />);
-   });
-});
