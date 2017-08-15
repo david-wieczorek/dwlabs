0392bcb6380d2980d2aab9200cf3d0e04a9440a1
David Wieczorek <blackratio@gmail.com>
2017-08-15 10:58:32 +0200

    change bloc style

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'leaflet/dist/leaflet.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
