1f62b6748b37ffa7416782297848cf98b8327460
David Wieczorek <blackratio@gmail.com>
2017-08-18 08:50:00 +0200

    add routes file and change app dest

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, HashRouter } from 'react-router-dom';
-import Main from './views';
-import Home from './views/home';
-import Resumeview from './views/resume';
-import Codeview from './views/code';
 import registerServiceWorker from './registerServiceWorker';
+
+import Routes from './routes';
+
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
 
-ReactDOM.render(
-  <HashRouter>
-    <Main>
-      <Route exact path="/" component={Home} />
-      <Route path="/resume" component={Resumeview} />
-      <Route path="/codes" component={Codeview} />
-    </Main>
-  </HashRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
